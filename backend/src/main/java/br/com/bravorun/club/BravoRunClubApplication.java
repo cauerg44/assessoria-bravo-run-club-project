f67e0c439e8a919a45752acfccf434f0fb204bcd
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-04-08 14:42:01 -0300

    First commit

---
+++ backend/src/main/java/br/com/bravorun/club/BravoRunClubApplication.java
@@ -0,0 +1,13 @@
+package br.com.bravorun.club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravoRunClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BravoRunClubApplication.class, args);
+	}
+
+}
