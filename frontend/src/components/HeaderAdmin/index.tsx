93fa23b6f94c3bf94ced57113feb34d26d972206
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-04-23 14:42:15 -0300

    feat-frontend: header link to admin home page

--- frontend/src/components/HeaderAdmin/index.tsx
+++ frontend/src/components/HeaderAdmin/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default function HeaderAdmin() {
   return (
     <header className='brc-header-admin'>
       <nav className='brc-container-1100 brc-nav-header-admin'>
-        <h2>Área administrativa</h2>
+        <Link to={"/admin"}>
+          <h2>Área administrativa</h2>
+        </Link>
         <h4>Olá, Admin!</h4>
       </nav>
     </header>
